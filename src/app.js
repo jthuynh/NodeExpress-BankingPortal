e6f176652adc379d45b7bbb662337c8004507d04
Jennifer Huynh <jntlhuynh@gmail.com>
2022-06-30 16:27:18 -0700

    added handling form data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { ap } = require('ramda');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.set('view engine', 'ejs');
 
 
 app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.urlencoded({ extended: true}));
 
 const accountData = fs.readFileSync('src/json/accounts.json','utf8');
 const accounts = JSON.parse(accountData);
@@ -43,8 +45,44 @@ app.get('/profile', function(req, res) {
         user: users[0]
     })
 })
+
+app.get('/transfer', function(req, res) {
+    res.render('transfer');
+})
+
+app.get('/payment', (req, res) => {
+    res.render('payment', {
+        account: accounts.credit
+    });
+})
+
 const userData = fs.readFileSync('src/json/users.json', 'utf8');
 const users = JSON.parse(userData);
+
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += parseInt(req.body.amount);
+
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+    res.render('transfer', {
+        message: "Transfer Completed"
+    })
+});
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+    res.render('payment', {
+        message: "Payment Successful",
+        account: accounts.credit
+    })
+})
 
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!');
