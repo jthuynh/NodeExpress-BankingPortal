17adab11aefdb66833fee479a2b693ea459f9dc7
Jennifer Huynh <jntlhuynh@gmail.com>
2022-06-29 23:53:49 -0700

    add file handling and routing

--- src/app.js
+++ src/app.js
@@ -10,9 +10,41 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname,'/public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json','utf8');
+const accounts = JSON.parse(accountData);
+
 app.get('/', function(req, res) {
-    res.render('index', {title:'Index'});
+    res.render('index', {
+        title:'Account Summary',
+        accounts: accounts
+    });
 });
+
+app.get('/savings', function(req, res) {
+    res.render('account', {
+        account: accounts.savings
+    })
+});
+
+app.get('/checking', function(req, res) {
+    res.render('account', {
+        account: accounts.checking
+    })
+});
+
+app.get('/credit', function(req, res) {
+    res.render('account', {
+        account: accounts.credit
+    })
+});
+
+app.get('/profile', function(req, res) {
+    res.render('profile', {
+        user: users[0]
+    })
+})
+const userData = fs.readFileSync('src/json/users.json', 'utf8');
+const users = JSON.parse(userData);
 
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!');
