8f32bc04dbe9095ed730dbcb6972fb3cc4a1dab7
Jennifer Huynh <jntlhuynh@gmail.com>
2022-06-30 16:34:12 -0700

    created a data access library

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { ap } = require('ramda');
+const { accounts, users, writeJSON } = require('./data');
 
 const app = express();
 
@@ -11,9 +11,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname,'/public')));
 app.use(express.urlencoded({ extended: true}));
-
-const accountData = fs.readFileSync('src/json/accounts.json','utf8');
-const accounts = JSON.parse(accountData);
 
 app.get('/', function(req, res) {
     res.render('index', {
@@ -56,15 +53,12 @@ app.get('/payment', (req, res) => {
     });
 })
 
-const userData = fs.readFileSync('src/json/users.json', 'utf8');
-const users = JSON.parse(userData);
 
 app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance -= req.body.amount;
     accounts[req.body.to].balance += parseInt(req.body.amount);
 
-    const accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
 
     res.render('transfer', {
         message: "Transfer Completed"
@@ -75,8 +69,7 @@ app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount);
 
-    const accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
 
     res.render('payment', {
         message: "Payment Successful",
